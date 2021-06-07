4aae0ec40120480cb6466d40a118f6cdbc26880d
hmduisuri <hmduisuri@gmail.com>
2021-06-07 10:25:08 +0900

    implimenting quiz card

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import QuizCard from './components/QuizCard';
 
 const App = () => {
+
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [quesNumber, setQuesNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
 
   const startTrivia = async() =>{
 
@@ -11,9 +20,21 @@ const App = () => {
   }
 
 
+  const nextQuestion = () => {
+
+  }
+
+
   return (
     <div className="App">
      Quiz
+     <button onClick={startTrivia}>Start game</button>
+     <p>Score:</p>
+     <p>Loading questions...</p>
+     {/* <QuizCard/> */}
+     <button onClick={nextQuestion}>Next Question</button>
+
+
     </div>
   );
 }
--- src/components/QuizCard.tsx
+++ src/components/QuizCard.tsx
@@ -1,11 +1,38 @@
 import React from 'react'
 
-const QuizCard = () => {
-    return (
-        <div>
-            Question card
-        </div>
-    )
+type prop = {
+    question : string;
+    answers: string[];
+    callBack: any;
+    userAnswer: boolean;
+    questionNr:number;
+    totalQuestions: number;
 }
+
+const QuizCard:React.FC<prop> = ({
+    question,
+    answers,
+    callBack,
+    userAnswer,
+    questionNr,
+    totalQuestions
+}) => (
+  
+        <div>
+            <p className='q__number'>
+                Question: {questionNr} / {totalQuestions}
+            </p>
+            <p dangerouslySetInnerHTML={{ __html:question }}></p>
+            <div>
+                {answers.map((answer) => (
+                    <div>
+                        <button disabled={userAnswer} onClick={callBack}>
+                        <span dangerouslySetInnerHTML={{__html:answer}}></span>
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </div>
+)
 
 export default QuizCard
